29268e69972536eed92b5f13f0f248e630aa5719
LiZeB <963899158@qq.com>
2019-08-10 11:25:44 +0800

    实现了散列表

---
+++ DataStructure/DataStructure/hash.h
@@ -0,0 +1,94 @@
+#ifndef hashChains_
+#define hashChains_
+
+#include<iostream>
+#include<utility> 
+#include<vector>
+#include<list>
+#include<string>
+#include<iterator>
+
+using namespace std;
+
+template<class K, class E>
+class hashChains
+{
+	/**************************************************************
+	哈希表：这里只实现了除留余数法构造哈希函数（只针对整数关键字）、分离链接法解决冲突；
+	因为这种组合比较经典；分离链接法解决冲突和探测法解决冲突两种实现难度上差不多。
+	在实际种如何构造哈希函数，使元素在整个哈希表种分布均匀是一个影响哈希表性能的很重要的因素。
+	另外哈希表的长度最好取为质数。
+	哈希表算法的优点是：查找效率高，时间复杂度是O(N),当然前提是在哈希表种分布均匀
+	****************************************************************/
+public:
+	hashChains(int theDivisor = 11){
+		divisor = theDivisor;
+		dSize = 0;
+
+		table = new list<pair<const K, E>>[divisor];
+	}
+
+	~hashChains() { delete[] table; }
+
+	bool empty() const { return dSize == 0; }
+	int size() const { return dSize; }
+
+	pair<const K, E>&  find(const K& theKey) const{
+		int index = (int)hash(theKey) % divisor;
+		for (auto element : table[index]) {
+			if (element.first == theKey) {
+				return element;
+			}
+		}
+		return pair<const K, E>();
+	}
+
+	void insert(const pair<const K, E>& thePair){
+		int homeBucket = hash(thePair.first) % divisor;
+		table[homeBucket].insert(table[homeBucket].begin(), thePair);
+		dSize++;	
+	}
+
+	void erase(const K& theKey){
+		int index = hash(theKey) % divisor;
+		auto iter = table[index].begin();
+		for (int i = 0; i < table[index].size();i++) {
+			if (theKey == iter ->first) {
+				table[index].erase(iter);
+				break;
+			}
+			advance(iter, 1);    //一定要注意这个advance()函数是迭代器增加指定大小的用法
+		}
+	}
+
+	void output(ostream& out) const{
+		for (int i = 0; i < divisor; i++)
+			if (table[i].size() == 0)
+				cout << "NULL" << endl;
+			else {
+				for (auto element : table[i]) {
+					cout << element.first << " " << element.second << " ";
+				}
+				/*auto iter = table[i].begin();      //这里只是为了测试advance的用法，才写的，但是不能正确运行，原因还不清楚
+				for (int i = 0;i < table[i].size()-1;i++) {
+					cout << iter->first << " " << iter->second << " ";
+					advance(iter, 1);
+				}*/
+				cout << endl;
+			}
+	}
+
+
+protected:
+	//定义哈希函数，
+	int hash(const K &theKey) const {
+		/**********************************
+		哈希函数：除留余数法
+		***********************************/
+		return theKey % divisor;
+	}
+	list<pair<const K, E>> *table;   //哈希表
+	int dSize;    //元素个数             
+	int divisor;  //哈希表长度
+};
+#endif
--- DataStructure/DataStructure/main.cpp
+++ DataStructure/DataStructure/main.cpp
@@ -1,89 +1,132 @@
 #include "forward_list.cpp"
 #include "stack.h"
 #include "linkedBinaryTree.cpp"
+#include "hash.h"
 
 using namespace std;
 
 int main() {
-	/*********测试链表的代码**********/
-	chain<int> linkList;
+	///*********测试链表的代码**********/
+	//chain<int> linkList;
 
-	//测试链表的插入
-	for (int i = 0; i < 14; i++) {
-		linkList.insert(i, 17-i);
-	}
-	linkList.output(cout);
+	////测试链表的插入
+	//for (int i = 0; i < 14; i++) {
+	//	linkList.insert(i, 17-i);
+	//}
+	//linkList.output(cout);
 
-	//测试链表的拷贝构造函数
-	chain<int> linkList2(linkList);
-	linkList2.output(cout);
+	////测试链表的拷贝构造函数
+	//chain<int> linkList2(linkList);
+	//linkList2.output(cout);
 
-	//测试链表的获取元素的函数
-	int a1 = 6;
-	int temp = linkList2.get(a1);
-	cout <<"按位置"<<a1<<"进行索引:"<< temp << endl;
+	////测试链表的获取元素的函数
+	//int a1 = 6;
+	//int temp = linkList2.get(a1);
+	//cout <<"按位置"<<a1<<"进行索引:"<< temp << endl;
 
-	//测试根据元素值返回元素值得索引
-	int temp2 = linkList2.indexOf(13);
-	cout <<"取某一个元素值"<<13<<"的索引：" <<temp2 << endl;
+	////测试根据元素值返回元素值得索引
+	//int temp2 = linkList2.indexOf(13);
+	//cout <<"取某一个元素值"<<13<<"的索引：" <<temp2 << endl;
 
-	//测试根据索引删除某一结点
-	linkList2.erase(13);
-	cout << "删除索引位置" << 13 << "的元素：" << endl;
-	linkList2.output(cout);
+	////测试根据索引删除某一结点
+	//linkList2.erase(13);
+	//cout << "删除索引位置" << 13 << "的元素：" << endl;
+	//linkList2.output(cout);
 
-	//测试用箱子排序算法对链表进行排序
-	linkList2.insert(5, 118);
-	linkList2.output(cout);
-	linkList2.insert(2, 18);
-	linkList2.output(cout);
-	linkList2.binSort(linkList2.getMax()+1);
-	linkList2.output(cout);
+	////测试用箱子排序算法对链表进行排序
+	//linkList2.insert(5, 118);
+	//linkList2.output(cout);
+	//linkList2.insert(2, 18);
+	//linkList2.output(cout);
+	//linkList2.binSort(linkList2.getMax()+1);
+	//linkList2.output(cout);
 
-	/***********测试栈的代码***********/
-	linkedStack<int> stack1;
+	///***********测试栈的代码***********/
+	//linkedStack<int> stack1;
 
-	for (int j = 0; j < 10; j++) {
-		stack1.push(j);
-	}
-	stack1.push(100);
-	cout << "栈的长度："<<stack1.size()<<endl;
-	while (!stack1.empty()) {
-		cout << stack1.top() << " ";
-		stack1.pop();
-	}
-	cout << endl;
-	
-	/***************测试二叉树的代码*****************/
-	linkedBinaryTree<int> a, x, y, z;
-	y.makeTree(1, a, a);
-	z.makeTree(2, a, a);
-	x.makeTree(3, y, z);
-	y.makeTree(4, x, a);
-	cout << "Number of nodes = ";
-	cout << y.size() << endl;
-	cout << "height = ";
-	cout << y.height() << endl;
-	cout << "Preorder sequence is ";
-	y.preOrderOutput();
-	cout << "Inorder sequence is ";
-	y.inOrderOutput();
-	cout << "Postorder sequence is ";
-	y.postOrderOutput();
-	cout << "Levelorder sequence is ";
-	y.levelOrderOutput();
+	//for (int j = 0; j < 10; j++) {
+	//	stack1.push(j);
+	//}
+	//stack1.push(100);
+	//cout << "栈的长度："<<stack1.size()<<endl;
+	//while (!stack1.empty()) {
+	//	cout << stack1.top() << " ";
+	//	stack1.pop();
+	//}
+	//cout << endl;
+	//
+	///***************测试二叉树的代码*****************/
+	//linkedBinaryTree<int> a, x, y, z;
+	//y.makeTree(1, a, a);
+	//z.makeTree(2, a, a);
+	//x.makeTree(3, y, z);
+	//y.makeTree(4, x, a);
+	//cout << "Number of nodes = ";
+	//cout << y.size() << endl;
+	//cout << "height = ";
+	//cout << y.height() << endl;
+	//cout << "Preorder sequence is ";
+	//y.preOrderOutput();
+	//cout << "Inorder sequence is ";
+	//y.inOrderOutput();
+	//cout << "Postorder sequence is ";
+	//y.postOrderOutput();
+	//cout << "Levelorder sequence is ";
+	//y.levelOrderOutput();
 
-	/****************测试二叉搜索树*******************/
-	binarySearchTree<int, char> y1;
-	y1.insert(pair<int, char>(1, 'a'));
-	y1.insert(pair<int, char>(6, 'c'));
-	y1.insert(pair<int, char>(4, 'b'));
-	y1.insert(pair<int, char>(8, 'd'));
-	cout << "Tree size is " << y1.size() << endl;
-    
-	int a_1 = 4;
-	y1.erase(a_1);
-	cout << "Tree size is " << y1.size() << endl;
+	///****************测试二叉搜索树*******************/
+	//binarySearchTree<int, char> y1;
+	//y1.insert(pair<int, char>(1, 'a'));
+	//y1.insert(pair<int, char>(6, 'c'));
+	//y1.insert(pair<int, char>(4, 'b'));
+	//y1.insert(pair<int, char>(8, 'd'));
+	//cout << "Tree size is " << y1.size() << endl;
+ //   
+	//int a_1 = 4;
+	//y1.erase(a_1);
+	//cout << "Tree size is " << y1.size() << endl;
+
+	/*****************测试哈希算法************************/
+	//针对<int, int>类型
+	hashChains<int, int> z;
+	z.insert({ 1,1 });
+
+	pair<int, int> p;
+	p.first = 2; p.second = 10;
+	z.insert(p);
+	p.first = 10; p.second = 50;
+	z.insert(p);
+	p.first = 24; p.second = 120;
+	z.insert(p);
+	p.first = 32; p.second = 160;
+	z.insert(p);
+	p.first = 3; p.second = 15;
+	z.insert(p);
+	p.first = 12; p.second = 60;
+	z.insert(p);
+	p.first = 23; p.second = 90;
+	z.insert(p);
+	p.first = 34; p.second = 120;
+	z.insert(p);
+	z.output(cout);
+	cout << "哈希表中元素的个数："<<z.size() << endl;
+	int key = 12;
+	p = z.find(key);
+	cout << "关键字:"<< key <<p.first << " " << p.second << endl;
+	z.erase(1);
+	cout << "删除关键字为1的元素：" << endl;
+	z.output(cout);
+
+	//针对<int, string>类型
+	hashChains<int, string> x;
+	x.insert({ 15, "LiZeBin" });
+	x.insert({ 21, "DongZan" });
+	x.insert({ 24, "LiuYuanDu" });
+	cout << "删除之前是：" << endl;
+	x.output(cout);
+	cout << "删除之后是：" << endl;
+	x.erase(24);
+	x.output(cout);
 
 	getchar();
 	return 0;
